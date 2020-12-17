c35fd1b30462e7748ab38afd3e6e982256c89419
leejevon <564331730@qq.com>
2020-12-17 12:11:48 +0800

    01-调整页面的基本样式

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 //项目根组件   路由配置页面
 
 import React, { Component } from 'react'
+import {HashRouter, BrowserRouter, Route, Link} from 'react-router-dom'
 
-export default class App extends Component {
-  render() {
+//导入页面组件
+import Home from './pages/Home'
+import CityList from './pages/CityList'
+
+
+export default function App() {
     return (
-      <div>
-        首页
-      </div>
+      <HashRouter>
+        {/* <Button type="primary">点我</Button> */}
+
+        {/* 导航 */}
+        <ul>
+          <li>
+          <Link to='/home'>首页</Link>
+          </li>
+          <li>
+          <Link to='/citylist'>城市选择页面</Link>
+          </li>
+        </ul>
+
+        {/* 路径和页面组件的关系 */}
+        <Route path="/home" component={Home} />
+        <Route path="/citylist" component={CityList} />
+      </HashRouter>
     )
-  }
 }
