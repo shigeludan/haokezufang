731b5af84c930532f3758b2f3a6dc83bccb76a41
leejevon <564331730@qq.com>
2020-12-17 14:36:45 +0800

    测试

---
+++ src/pages/CityList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CityList extends Component {
+    render() {
+        return (
+            <div>
+                城市选择的页面
+                {/* <h3>页面</h3> */}
+            </div>
+        )
+    }
+}
